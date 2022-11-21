114d089818e925a11f87ffe9dd3bf1aba3e8c93c
rconinx <robbe.coninx@outlook.com>
2022-11-21 12:39:13 +0100

    Created an age calculator

--- calculations.js
+++ calculations.js
@@ -6,7 +6,28 @@ submitButton.addEventListener('click', function () {
     console.log(city);
     let url = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/" + city + "?unitGroup=metric&include=current&key=P9J7ZGZ9SBGSZQTDBMT86WNN3&contentType=json";
     console.log(url);
-    fetch (url)
-.then(x => x.text())
-.then(y => document.getElementById("test").innerHTML = y);
+    //fetch (url)
+//.then(x => x.text())
+//.then(y => document.getElementById("weather").innerHTML = y);
+
+
+
+
+
+
+
+
+
+
+let bdate = document.getElementById('date').value;
+let today = new Date();
+    let birthDate = new Date(bdate);
+    let age = today.getFullYear() - birthDate.getFullYear();
+    let m = today.getMonth() - birthDate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+        age--;
+    }
+    console.log(age);
 });
+
+
