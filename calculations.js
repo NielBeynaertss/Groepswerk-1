56ec34bf641d398288186ab072d50e692db718b9
rconinx <robbe.coninx@outlook.com>
2022-11-21 11:54:14 +0100

    API is compatible with input from City

--- calculations.js
+++ calculations.js
@@ -1,6 +1,12 @@
+let submitButton = document.getElementById('submitButton');
 
+submitButton.addEventListener('click', function () {
     // fetch van jsonplaceholder API;
-    let url = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/brussels?unitGroup=metric&include=current&key=P9J7ZGZ9SBGSZQTDBMT86WNN3&contentType=json";
-fetch (url)
+    let city = document.getElementById('city').value;
+    console.log(city);
+    let url = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/" + city + "?unitGroup=metric&include=current&key=P9J7ZGZ9SBGSZQTDBMT86WNN3&contentType=json";
+    console.log(url);
+    fetch (url)
 .then(x => x.text())
 .then(y => document.getElementById("test").innerHTML = y);
+});
